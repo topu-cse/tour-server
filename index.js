790f4ee8f673c988c9979a683ac9966fde2080dd
topu12345 <topu15-3604@diu.edu.bd>
2022-11-10 16:09:22 +0600

    delete

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
 try{
      const serviceCollection=client.db('DreamTour').collection('services');
-     const reviewCollection=client.db('DreamTour').collection('review')
+     const reviewCollection=client.db('DreamTour').collection('reviews')
 
      app.get('/services',async(req,res)=>{
         const query={}
@@ -35,6 +35,32 @@ app.get('/services/:id',async(req,res)=>{
     const query={_id:ObjectId(id)};
     const service= await serviceCollection.findOne(query)
     res.send(service);
+})
+
+//review api
+
+
+//reviewshow
+app.get('/reviews',async(req,res)=>{
+    
+    let query={}
+    if(req.query.email){
+        query={
+            email:req.query.email
+        }
+    }
+    cursor=reviewCollection.find(query);
+    const reviews= await cursor.toArray()
+    res.send(reviews);
+})
+
+
+
+
+app.post('/reviews',async(req,res)=>{
+    const review=req.body;
+    const result =await reviewCollection.insertOne(review);
+    res.send(result);
 })
 
 }
